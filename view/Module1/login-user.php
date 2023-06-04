ca6100cdbd9c8d521395b439950678bb12ff0f69
arianazreen <120695290+arianazreen@users.noreply.github.com>
2023-06-04 23:55:35 +0800

    login for admin & user

---
+++ view/Module1/login-user.php
@@ -0,0 +1,163 @@
+<?php
+
+	$error = false; 
+
+   	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		include 'database.php';
+
+		//username and password sent from form 
+		$username = $_POST["username"]; 
+		$password = $_POST["password"]; 
+		
+		$username = mysqli_real_escape_string($conn,$_POST['username']);
+		$password = mysqli_real_escape_string($conn,$_POST['password']); 
+		
+		$sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+		$result = mysqli_query($conn,$sql);
+		$row = mysqli_fetch_array($result);
+		//$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+		//$active = $row['active'];
+
+		echo "<script>alert('You are now logged into the student account !'); window.location='manage-user.php'</script>";
+
+	  	if($row['role'] == "student")
+		{
+			$_SESSION['username'] = $username;
+			echo "<script>alert('You are now logged into the student account !'); window.location='manage-user.php'</script>";
+		}
+		else if($row['role'] == "expert")
+		{
+			$_SESSION['username'] = $username;
+			echo "<script>alert('You are now logged into the expert account !'); window.location='manage-user.php'</script>";
+		}
+		else
+		{
+			$_SESSION['status'] = "Username / Password is Invalid";
+			echo "<script>alert('Invalid username / password'); window.location='manage-user.php'</script>";
+		}
+	}
+      
+    	/*$count = mysqli_num_rows($result);
+      
+		if($count == 1)
+		{
+			//session_register("username");
+			$_SESSION['loginBtn'] = $username;
+			
+			header("location: index.php");
+		}
+		else
+		{
+			$error = "Your Login Username or Password is invalid";
+		}
+   }*/ 
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Bootstrap 4 Admin &amp; Dashboard Template">
+	<meta name="author" content="Bootlab">
+
+	<title>Fk-EduSearch Login</title>
+	<link rel="shortcut icon" href="../../dist/img/logo/fk-edusearch-border.png" type="image/x-icon">
+
+	<!-- Link to CSS Files -->
+    <link href="../../dist/css/modern.css" rel="stylesheet">
+	<link href="css-m1/login-user-style.css" rel="stylesheet">
+
+	<style>
+		body {
+			opacity: 0;
+			background-color: #07A492;
+		}
+	</style>
+    <script src="../../dist/js/settings.js"></script>
+</head>
+
+<body class="theme-blue">
+
+    <?php
+		if($error)
+		{
+			echo ' <div class="alert alert-danger 
+				alert-dismissible fade show" role="alert"> 
+			<strong>Error!</strong> '. $error.'
+		
+			<button type="button" class="close" 
+				data-dismiss="alert aria-label="Close">
+				<span aria-hidden="true">×</span> 
+			</button> 
+			</div> '; 
+		}
+	?>
+
+	<div class="splash active">
+		<div class="splash-icon"></div>
+	</div>
+
+	<main class="main h-100 w-100">
+		<div class="container h-100">
+			<div class="row h-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+						<div class="card" style="margin-top: 90px; border-radius: 20px;">
+							<div class="card-body">
+								<div class="m-sm-4">
+									<div class="text-center">
+										<img src="../../dist/img/logo/fk-edusearch-border.png" alt="FK-EduSearch Logo" class="img-fluid rounded-circle" width="90" />
+									</div>
+                                    <div class="text-center mt-4">
+                                        <b><h1>LOGIN</h1></b>
+                                    </div>
+                                    <div class="spacing"></div>
+									<form method="post" action="login.php">
+										<div class="mb-3">
+												<i class="align-middle me-1 fa fa-fw fa-user" style="margin-left: 10px;"></i>
+												<input class="form-input" type="text" name="username" placeholder="USERNAME"/>
+										</div>
+										<div class="mb-3">
+                                            <i class="align-middle me-1 fa fa-fw fa-lock" style="margin-left: 10px;"></i>
+											<input class="form-input" type="password" name="password" placeholder="PASSWORD" />
+										</div>
+                                        <div class="spacing"></div>
+										<div class="text-center mt-3">
+											<button type="submit" class="button-m1">LOGIN</button>
+											<br>
+											<div class="spacing2" style="padding: 5px;"></div>
+											<small>
+												<a href="pages-reset-password.html">Forgot password?</a>
+											</small>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+							<div class="footer"><p>© 2023 - UNIVERSITI MALAYSIA PAHANG</p></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<svg width="0" height="0" style="position:absolute">
+		<defs>
+			<symbol viewBox="0 0 512 512" id="ion-ios-pulse-strong">
+				<path
+					d="M448 273.001c-21.27 0-39.296 13.999-45.596 32.999h-38.857l-28.361-85.417a15.999 15.999 0 0 0-15.183-10.956c-.112 0-.224 0-.335.004a15.997 15.997 0 0 0-15.049 11.588l-44.484 155.262-52.353-314.108C206.535 54.893 200.333 48 192 48s-13.693 5.776-15.525 13.135L115.496 306H16v31.999h112c7.348 0 13.75-5.003 15.525-12.134l45.368-182.177 51.324 307.94c1.229 7.377 7.397 11.92 14.864 12.344.308.018.614.028.919.028 7.097 0 13.406-3.701 15.381-10.594l49.744-173.617 15.689 47.252A16.001 16.001 0 0 0 352 337.999h51.108C409.973 355.999 427.477 369 448 369c26.511 0 48-22.492 48-49 0-26.509-21.489-46.999-48-46.999z">
+				</path>
+			</symbol>
+		</defs>
+	</svg>
+	<script src="../../dist/js/app.js"></script>
+
+</body>
+
+</html>
---
+++ view/Module1/login.php
@@ -0,0 +1,138 @@
+<?php
+
+$error = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include 'database.php';
+
+    // Retrieve form data
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // Perform validation
+    if (empty($username) || empty($password)) {
+        $error = true;
+        $errorMessage = "Username and password are required.";
+    } else {
+        $sql = "SELECT * FROM admin WHERE adminID = '$username'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result && mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            $hashedPassword = $row['adminPass'];
+
+            // Verify the password
+            if (password_verify($password, $hashedPassword)) {
+                session_start(); // Start the session
+                $_SESSION['username'] = $username;
+                header("Location: manage-user.php");
+                exit();
+            } else {
+                $error = true;
+                $errorMessage = "Invalid username or password.";
+            }
+        } else {
+            $error = true;
+            $errorMessage = "Invalid username or password.";
+        }
+    }
+}
+
+if ($error) {
+    echo "<script>alert('$errorMessage'); window.location='login.php'</script>";
+    exit();
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Bootstrap 4 Admin &amp; Dashboard Template">
+	<meta name="author" content="Bootlab">
+
+	<title>Fk-EduSearch ADMIN Login</title>
+	<link rel="shortcut icon" href="../../dist/img/logo/fk-edusearch-border.png" type="image/x-icon">
+
+	<!-- Link to CSS Files -->
+    <link href="../../dist/css/modern.css" rel="stylesheet">
+	<link href="css-m1/login-style.css" rel="stylesheet">
+
+	<style>
+		body {
+			opacity: 0;
+			background-color: #1D2F3A;
+		}
+	</style>
+    <script src="../../dist/js/settings.js"></script>
+</head>
+
+<body class="theme-blue">
+	<div class="splash active">
+		<div class="splash-icon"></div>
+	</div>
+
+	<main class="main h-100 w-100">
+		<div class="container h-100">
+			<div class="row h-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+						<div class="card" style="margin-top: 90px; border-radius: 20px;">
+							<div class="card-body">
+								<div class="m-sm-4">
+									<div class="text-center">
+										<img src="../../dist/img/logo/fk-edusearch-border.png" alt="FK-EduSearch Logo" class="img-fluid rounded-circle" width="90" />
+									</div>
+                                    <div class="text-center mt-4">
+                                        <b><h1>ADMIN LOGIN</h1></b>
+                                    </div>
+                                    <div class="spacing"></div>
+									<form method="post" action="login.php">
+										<div class="mb-3">
+												<i class="align-middle me-1 fa fa-fw fa-user" style="margin-left: 10px;"></i>
+												<input class="form-input" type="text" name="username" placeholder="USERNAME"/>
+										</div>
+										<div class="mb-3">
+                                            <i class="align-middle me-1 fa fa-fw fa-lock" style="margin-left: 10px;"></i>
+											<input class="form-input" type="password" name="password" placeholder="PASSWORD" />
+										</div>
+                                        <div class="spacing"></div>
+										<div class="text-center mt-3">
+											<button type="submit" class="button-m1">LOGIN</button>
+											<br>
+											<div class="spacing2" style="padding: 5px;"></div>
+											<small>
+												<a href="pages-reset-password.html">Forgot password?</a>
+											</small>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+							<div class="footer"><p>© 2023 - UNIVERSITI MALAYSIA PAHANG</p></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<svg width="0" height="0" style="position:absolute">
+		<defs>
+			<symbol viewBox="0 0 512 512" id="ion-ios-pulse-strong">
+				<path
+					d="M448 273.001c-21.27 0-39.296 13.999-45.596 32.999h-38.857l-28.361-85.417a15.999 15.999 0 0 0-15.183-10.956c-.112 0-.224 0-.335.004a15.997 15.997 0 0 0-15.049 11.588l-44.484 155.262-52.353-314.108C206.535 54.893 200.333 48 192 48s-13.693 5.776-15.525 13.135L115.496 306H16v31.999h112c7.348 0 13.75-5.003 15.525-12.134l45.368-182.177 51.324 307.94c1.229 7.377 7.397 11.92 14.864 12.344.308.018.614.028.919.028 7.097 0 13.406-3.701 15.381-10.594l49.744-173.617 15.689 47.252A16.001 16.001 0 0 0 352 337.999h51.108C409.973 355.999 427.477 369 448 369c26.511 0 48-22.492 48-49 0-26.509-21.489-46.999-48-46.999z">
+				</path>
+			</symbol>
+		</defs>
+	</svg>
+	<script src="../../dist/js/app.js"></script>
+
+</body>
+
+</html>
