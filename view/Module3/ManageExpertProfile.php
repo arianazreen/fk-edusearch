728fd4eec191a3ba471a3f25a419f296c889f0ef
LAPTOP-C9C2EPS6\HP <nurinnasya17@gmail.com>
2023-06-10 23:43:55 +0800

    Update

---
+++ view/Module3/ManageExpertProfile.php
@@ -0,0 +1,500 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Responsive Bootstrap 4 Admin &amp; Dashboard Template">
+    <meta name="author" content="Bootlab">
+  
+    <title>Manage Expert Profile</title>
+    <link rel="stylesheet" href="../../dist/css/modern.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+    <style>
+      body {
+        opacity: 0;
+      }
+    </style>
+    <script src="../../dist/js/settings.js"></script>
+    <!-- END SETTINGS -->
+  </head>
+  
+
+  <body>
+    <div class="wrapper">
+      <!-- CONTENT -->
+      <div class="main">
+        <nav class="navbar navbar-expand navbar-theme">
+          <div class="container-fluid">
+            <!--Nav - Logo-->
+            <img src="./imageM3/fk-edusearch-logo.png" alt="imageM3" style="width: 35px; height: 35px;" >
+            <!--Nav - Home (name) -->
+            <a class="navbar-brand" href="#">FK-EDUSEARCH</a>&nbsp;
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!--Nav - Home -->
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="./M2-user_homepage.php">Home</a>
+                </li>
+              </ul>
+            </div>
+            
+          <!--Nav - Notification -->
+          <div class="navbar-collapse collapse">
+            <ul class="navbar-nav ms-auto mt-2">
+              <li class="nav-item dropdown ms-lg-2">
+                <a
+                  class="nav-link dropdown-toggle position-relative"
+                  href="#"
+                  id="alertsDropdown"
+                  data-bs-toggle="dropdown"
+                >
+                  <i class="align-middle fas fa-bell"></i>
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+                  aria-labelledby="alertsDropdown"
+                >
+                  <div class="dropdown-menu-header">2 New Notifications</div>
+                  <div class="list-group">
+                    <a href="#" class="list-group-item">
+                      <div class="row g-0 align-items-center">
+                        <div class="col-2">
+                          <i
+                            class="ms-1 text-success fas fa-fw fa-bell-slash"
+                          ></i>
+                        </div>
+                        <div class="col-10">
+                          <div class="text-dark">New Notifications</div>
+                          <div class="text-muted small mt-1">
+                          <h2 class="card-title mb-0"> What theoretical framework could be used to study the perception of pre-service teacher students about artificial intelligence?</h2>
+                          <p>Artificial intelligence (AI) refers to the theory and development of computer systems to perform tasks that normally require human intelligence. Because of the massive, often quite unintelligible publicity that it gets, artificial intelligence is almost completely misunderstood by individuals inside the field of Education. ....</p>
+                          </div>
+                          <div class="text-muted small mt-1">4h ago</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="dropdown-menu-footer">
+                    <a href="./Inbox.php" class="text-muted">Show all notifications</a>
+                  </div>
+                </div>
+              </li>
+
+              <!--Nav- Profile 
+              <div class="post-box border-0">
+                        <img class="profile-img" src="../../dist/img/avatars/nurul_najwa.jpg" alt="Profile Image" 
+                         style="width: 35px; height: 25px;" >
+                         <h6>Nurul Najwa</h6>
+                         <p>Student</p>          
+              </div>-->
+
+              <div class="row mt-3 mx-2">
+                <div class="col-sm-4">
+                <img src="./imageM3/profilecirclenew.png" alt="imageM3" style="width: 35px; height: 35px;" >
+                </div>
+                <div class="col-sm-8">
+                      <h6 class="mb-0" style="color: #fff;">Dr.Muaz bin Rizal</h6>
+                      <p style="color: #BBE3E5;">Expert</p>
+                </div>
+              </div>
+
+              <!--Nav - Dropdown Setting -->
+              <li class="nav-item dropdown ms-lg-2">
+                <a
+                  class="nav-link dropdown-toggle position-relative"
+                  href="#"
+                  id="userDropdown"
+                  data-bs-toggle="dropdown"
+                >
+                  <i class="align-middle fas fa-cog"></i>
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="userDropdown"
+                >
+                  <a class="dropdown-item"></a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="/Profile"
+                    ><i class="align-middle me-1 fas fa-fw fa-user"></i>
+                    Profile</a
+                  >
+                  <a class="dropdown-item" href="#"
+                    ><i class="align-middle me-1 fas fa-fw fa-cogs"></i> Report </a
+                  >
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="logout"
+                    ><i
+                      class="align-middle me-1 fas fa-fw fa-arrow-alt-circle-right"
+                    ></i>
+                    Sign out</a
+                  >
+                </div>
+              </li>
+            </ul>
+          </div>
+          </div>
+
+        </nav>
+
+        <!--Content -->
+        <main class="content">
+            <!--1st Row-->
+
+          <div class="container-fluid">
+              <div class="header">
+                <h1 class="header-title"> PROFILE </h1>
+              </div>
+              <div class="post-box">
+              <img src="./imageM3/profilecirclenew.png" alt="imageM3" style="width: 35px; height: 35px;" >
+            <div class="post-info">
+                    <div class="name"> Dr.Muaz bin Rizal </div>
+                    <div class="date"> Expert | Edit Profile </div>
+                  </div>
+                </div>
+
+                
+               <div class="row">
+                 <div class="col-12 col-lg-8">
+                   <div class="card flex-fill w-100">
+                      <div class="card-header">
+                       <h2 class="card-title"> Title </h2>
+                       <h6 class="card-subtitle text-muted">
+                    <button class="create-button btn btn-primary" style=" color: white; position: absolute; left:70%; margin-top:-2%; background-color: #07A492; font-weight: 400;">CREATE NEW</a>
+								      </h6>
+                         <!--<h6 class="card-subtitle text-muted">A line chart is a way of plotting data points on a line.</h6>-->
+                          <div>
+                            <!--Research Input-->
+                          <div class="input-container">
+                            <div class="input-box">
+                            <input type="text" placeholder="Research Name">
+                            </div> 
+                          </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-4">
+                    <div class="card flex-fill w-100">
+                      <div class="card-header">
+    
+                          <h2 class="card-title mb-0"> Publications History </h2>
+                          <p>
+                            <ul>
+                            <li> Emeging Trends in Computer System Security. </li>
+                            <li> Virtual Reality (VR) and Augmented Reality (AR) application. </li>
+                          </ul>
+                          </p>
+                      </div>
+                    </div>
+                </div>
+              </div>
+             
+  
+           <!--2nd Row-->
+
+           <div class="row">
+            <div class="col-12 col-lg-8">
+              <div class="card flex-fill w-100">
+                 <div class="card-header">
+                  <h2 class="card-title"> List of Publications </h2>
+                    <!--<h6 class="card-subtitle text-muted">A line chart is a way of plotting data points on a line.</h6>-->
+                    <div class="card-body d-flex">
+                      <div class="align-self-center w-100">
+                          <div class="document-wrapper">
+                            <!-- Document Container -->
+                            <div class="document-container">
+                              <!-- Create new button -->
+                            <button class="create-button btn btn-primary" onclick="createDocumentBox(this)">CREATE NEW</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+           <div class="col-6 col-lg-4">
+               <div class="card flex-fill w-100">
+                 <div class="card-header">
+
+                     <h2 class="card-title mb-0"> Social Media Accounts </h2>
+                     <p>
+                       <ul>
+                       <li> Bachelor in Computer System </li>
+                     </ul>
+                     </p>
+                 </div>
+               </div>
+           </div>
+         </div>
+         </div>
+         </div>
+         </div>
+      
+
+       </main>
+
+        <!--Footer-->
+        <footer class="footer">
+          <div class="container-fluid">
+            <div class="row text-muted">
+              <div class="col-8 text-start"></div>
+              <div class="col-4 text-end">
+                <p class="mb-0">&copy; 2023 - UNIVERSITI MALAYSIA PAHANG</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+
+    <svg width="0" height="0" style="position: absolute">
+      <defs>
+        <symbol viewBox="0 0 512 512" id="ion-ios-pulse-strong">
+          <path
+            d="M448 273.001c-21.27 0-39.296 13.999-45.596 32.999h-38.857l-28.361-85.417a15.999 15.999 0 0 0-15.183-10.956c-.112 0-.224 0-.335.004a15.997 15.997 0 0 0-15.049 11.588l-44.484 155.262-52.353-314.108C206.535 54.893 200.333 48 192 48s-13.693 5.776-15.525 13.135L115.496 306H16v31.999h112c7.348 0 13.75-5.003 15.525-12.134l45.368-182.177 51.324 307.94c1.229 7.377 7.397 11.92 14.864 12.344.308.018.614.028.919.028 7.097 0 13.406-3.701 15.381-10.594l49.744-173.617 15.689 47.252A16.001 16.001 0 0 0 352 337.999h51.108C409.973 355.999 427.477 369 448 369c26.511 0 48-22.492 48-49 0-26.509-21.489-46.999-48-46.999z"
+          ></path>
+        </symbol>
+      </defs>
+    </svg>
+    <script src="../../dist/js/app.js"></script>
+
+
+    <!--Script-->
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			// Pie chart
+			new Chart(document.getElementById("chartjs-dashboard-pie"), {
+				type: 'pie',
+				data: {
+					labels: ["Networking", "Software Engineering", "Multimedia and Graphics"],
+					datasets: [{
+						data: [15, 7, 14],
+						backgroundColor: [
+							"#43BCAE",
+							"#BBE3E5",
+							"#1A5D55",
+						],
+						borderColor: "transparent"
+					}]
+				},
+				options: {
+					responsive: !window.MSInputMethodContext,
+					maintainAspectRatio: false,
+					legend: {
+						display: false
+					},
+					cutoutPercentage: 75
+				}
+			});
+		});
+
+    // Get references to the button, input container, and input boxes
+    const createButton = document.querySelector(".create-button");
+    const inputContainer = document.querySelector(".input-container");
+    const inputBoxes = document.querySelectorAll(".input-box");
+
+    // Function to create a new input box
+    function createInputBox() {
+      const newInputBox = document.createElement("div");
+      newInputBox.classList.add("input-box");
+
+      const newInput = document.createElement("input");
+      newInput.type = "text";
+      newInput.placeholder = "Research Name";
+
+      newInputBox.appendChild(newInput);
+      inputContainer.appendChild(newInputBox);
+    }
+
+    // Add event listener to the button
+    createButton.addEventListener("click", createInputBox);
+
+    // Add event listener to the last input box for Enter key
+    const lastInputBox = inputBoxes[inputBoxes.length - 1];
+    lastInputBox.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        createInputBox();
+      }
+    });
+
+    // Get reference to the document container
+    const documentContainer = document.querySelector(".document-container");
+
+    // Function to create a new document box with button
+    function createDocumentBox(button) {
+      const newDocumentBox = document.createElement("div");
+      newDocumentBox.classList.add("document-box");
+
+      const newButton = document.createElement("button");
+      newButton.classList.add("create-button", "btn", "btn-primary");
+      newButton.textContent = "CREATE NEW DOCUMENT";
+      newButton.addEventListener("click", function() {
+        createDocumentBox(newButton);
+      });
+
+      const newButtonWrapper = document.createElement("div");
+      newButtonWrapper.appendChild(newButton);
+
+      const newDocumentContent = document.createElement("div");
+      newDocumentContent.textContent = "New Document";
+
+      newDocumentBox.appendChild(newButtonWrapper);
+      newDocumentBox.appendChild(newDocumentContent);
+      documentContainer.appendChild(newDocumentBox);
+
+      // Remove the button from the previous box
+      button.remove();
+
+       // Scroll to the right
+       documentContainer.scrollLeft = documentContainer.scrollWidth;
+    }
+	</script>
+
+
+
+     <!--css-->
+
+     <style>
+
+      .input-container {
+        margin-top: 20px;
+      }
+
+      .input-box {
+        margin-bottom: 10px;
+      }
+      
+      .document-container {
+      margin-top: 20px;
+      display: flex;
+      flex-wrap: nowrap; /* Prevent wrapping to the next line */
+      overflow-x: auto; /* Enable horizontal scrolling */
+      -ms-overflow-style: none; /* Hide scrollbar in Internet Explorer */
+      scrollbar-width: none; /* Hide scrollbar in Firefox */
+    }
+
+    .document-box {
+      position: relative;
+      display: inline-block;
+      margin-right: 10px;
+      margin-bottom: 10px;
+      border: 1px solid #ccc;
+      padding: 10px;
+      height: 150px;
+      width: 200px;
+    }
+
+    .document-wrapper {
+      margin-top: 10px;
+      position: relative;
+    }
+
+    
+        .search-box input[type="text"] {
+            flex-grow: 1;
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .post-box {
+            position: relative;
+            display: flex;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 10px;
+        }
+
+        .post-box .profile-img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+
+        .post-box .post-info {
+            flex-grow: 1;
+        }
+
+        .post-box .post-info .name {
+            font-weight: bold;
+            margin-bottom: 5px;
+            color: #fff;
+            font-size: 19px;
+        }
+
+        .post-box .post-info .date {
+            font-size: 12px;
+            color:#BBE3E5;
+        }
+
+        .post-box .actions {
+            display: flex;
+            align-items: center;
+            margin-top: 10px;
+        }
+
+        .post-box .actions .icon {
+            margin-right: 10px;
+            cursor: pointer;
+        }
+
+        .post-box .actions .edit-btn,
+        .post-box .actions .delete-btn {
+            padding: 5px 10px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .post-box .status {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            font-weight: bold;
+        }
+        .post-info {
+            flex-grow: 1;
+        }
+
+          .container-box {
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 10px;
+            margin-top: 10px;
+        }
+        .line {
+            border-top: 1px solid #ccc;
+        }
+          
+        label {
+    display: block;
+    font: 1rem 'Fira Sans', sans-serif;
+      }
+
+    input,
+    label {
+    margin: 0;
+    }
+
+     </style>
+  </body>
+</html>
