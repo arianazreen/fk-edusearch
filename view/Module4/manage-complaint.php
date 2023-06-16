a767827b25bf17f84357530985734d6f0169e2e9
ftnfaiqah <atien.yusof87@gmail.com>
2023-06-16 22:30:35 +0800

    updated

--- view/Module4/manage-complaint.php
+++ view/Module4/manage-complaint.php
@@ -37,25 +37,32 @@
 
 				<ul class="sidebar-nav">
 					<div class="dropdown-divider"></div>
+
 					<!-- REPORT -->
 					<li class="sidebar-item">
-						<a data-bs-target="#dashboards" data-bs-toggle="collapse" class="sidebar-link collapsed">
+						<a data-bs-target="#report" data-bs-toggle="collapse" class="sidebar-link collapsed">
 							<i class="align-middle me-2 fas fa-fw fa-file-contract"></i> <span class="align-middle">Report</span>
 						</a>
-						<ul id="dashboards" class="sidebar-dropdown list-unstyled collapse " data-bs-parent="#sidebar">
-							<li class="sidebar-item"><a class="sidebar-link" href="user-activity.php">User Activity</a></li>
-							<li class="sidebar-item"><a class="sidebar-link" href="">User Satisfication</a></li>
+						<ul id="report" class="sidebar-dropdown list-unstyled collapse " data-bs-parent="#sidebar">
+							<li class="sidebar-item"><a class="sidebar-link active" href="../Module4/user-activity.php">User Activity</a></li>
+							<li class="sidebar-item"><a class="sidebar-link" href="../Module1/system-performance.php">User Satisfication</a></li>
 						</ul>
 					</li>
+
 					<!-- MANAGE USER PROFILE -->
 					<li class="sidebar-item">
-						<a class="sidebar-link" href="#">
+						<a data-bs-target="#manageuser" data-bs-toggle="collapse" class="sidebar-link collapsed">
 							<i class="align-middle me-2 fas fa-fw fa-users-cog"></i> <span class="align-middle">Manage User Profile</span>
 						</a>
+						<ul id="manageuser" class="sidebar-dropdown list-unstyled collapse " data-bs-parent="#sidebar">
+							<li class="sidebar-item"><a class="sidebar-link" href="../Module1/manage-genUser.php">General User</a></li>
+							<li class="sidebar-item"><a class="sidebar-link" href="../Module1/manage-expert.php">Expert</a></li>
+						</ul>
 					</li>
+
 					<!-- MANAGE COMPLAINT -->
 					<li class="sidebar-item">
-						<a class="sidebar-link" href="#">
+						<a class="sidebar-link" href="../Module4/manage-complaint.php">
 							<i class="align-middle me-1 fas fa-fw fa-comments"></i> <span class="align-middle">Manage Complaint</span>
 						</a>
 					</li>
@@ -65,10 +72,10 @@
 							<i class="align-middle me-2 fas fa-fw fa-bell"></i> <span class="align-middle">Notifications</span>
 						</a>
 					</li>
-					<!-- <div class="dropdown-divider"></div> -->
+					
 					<!-- LOG OUT -->
 					<li class="sidebar-item" style="position: absolute; bottom: 10px; ">
-						<a class="sidebar-link" href="#">
+						<a class="sidebar-link" href="../Module1/logout-admin.php">
 							<i class="align-middle me-2 fas fa-fw fa-sign-out-alt"></i> <span class="align-middle">Log Out</span>
 						</a>
 					</li>
@@ -86,6 +93,9 @@
 
 			<main class="content">
 				<div class="container-fluid">
+
+					 <!-- echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>'; -->
+
 
 					<div class="header">
 						<h1 class="header-title">
@@ -110,7 +120,7 @@
 
 										<?php
 											// connect to the database
-											include("../Module1/database.php");
+											include '../Module1/database.php';
 
 											// to view complaint details
 											
